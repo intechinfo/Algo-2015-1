dcb36da6f8246a57bcfebba9208ddb0b8c94e743
Olivier Spinelli <olivier.spinelli@invenietis.com>
2015-07-17 17:03:47 +0200

    Parser Start

---
+++ Analyse/ITI.Parser/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.Parsing
+{
+    public class Class1
+    {
+    }
+}
