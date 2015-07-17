f79eaa4ec7b997d35f62433dd1866dd935a7af5e
Olivier Spinelli <olivier.spinelli@invenietis.com>
2015-07-17 10:51:55 +0200

    Start of tokenizer

---
+++ Analyse/ITI.Tokenizer/TokenType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.Parsing
+{
+    [Flags]
+    public enum TokenType
+    {
+        None = 0,
+        IsAddOperator = 1,
+        Plus = IsAddOperator,
+        Minus = IsAddOperator + 2,
+        IsMultOperator = 4,
+        Mult = IsMultOperator,
+        Div = IsMultOperator + 2,
+        Number = 8,
+        OpenPar = 16,
+        ClosePar = 32,
+        EndOfInput = 64,
+        Error = 128
+    }
+}
---
+++ Analyse/ITI.Tokenizer/Tokenizer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.Parsing
+{
+    public class Tokenizer
+    {
+        IEnumerator<char> _input;
+        int _pos;
+        int _maxPos;
+        TokenType _type;
+
+        public Tokenizer( string s )
+            : this( s, 0, s.Length )
+        {
+        }
+
+        public Tokenizer( string s, int startIndex )
+            : this( s, startIndex, s.Length )
+        {
+        }
+
+        public Tokenizer( string s, int startIndex, int count )
+            : this( (IEnumerable<char>)s, startIndex, count )
+        {
+        }
+
+        public Tokenizer( IEnumerable<char> input, int startIndex, int count )
+        {
+            if( input == null ) throw new ArgumentNullException();
+            if( count < 0 ) throw new ArgumentException();
+            _input = input.GetEnumerator();
+            _type = TokenType.None;
+            _pos = startIndex;
+            _maxPos = startIndex + count;
+        }
+
+        public bool MatchInteger( int expected )
+        {
+
+        }
+
+        public bool MatchInteger( out int value )
+        {
+
+        }
+
+    }
+}
