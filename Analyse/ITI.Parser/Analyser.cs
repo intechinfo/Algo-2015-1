95a106a2ec4312069dda4bfad88af6aac87e79c6
Olivier Spinelli <olivier.spinelli@invenietis.com>
2015-07-20 09:58:59 +0200

    Start parsing...

--- Analyse/ITI.Parser/Analyser.cs
+++ Analyse/ITI.Parser/Analyser.cs
@@ -8,10 +8,32 @@ namespace ITI.Parsing
 {
     public class Analyser
     {
-        
-        public Node Analyse( string s )
+
+        public Node Expression( string s )
+        {
+            return Expression( new Tokenizer( s ) );
+        }
+
+        public Node Expression( Tokenizer tokenizer )
         {
             return null;
+        }
+
+        public Node Factor( Tokenizer tokenizer )
+        {
+            if( tokenizer.CurrentToken == TokenType.Number ) return Constant( tokenizer );
+            if( !tokenizer.Match( TokenType.OpenPar ) ) return new ErrorNode( "Expected constant or opening parenthesis." );
+            Node e = Expression( tokenizer );
+            if( e is ErrorNode ) return e;
+            if( !tokenizer.Match( TokenType.ClosePar ) ) return new ErrorNode( "Expected closing parenthesis." );
+            return e;
+        }
+
+        public Node Constant( Tokenizer tokenizer )
+        {
+            int value;
+            if( !tokenizer.MatchInteger( out value ) ) return new ErrorNode( "Constant expected." );
+            return new ConstantNode( value );
         }
 
     }
