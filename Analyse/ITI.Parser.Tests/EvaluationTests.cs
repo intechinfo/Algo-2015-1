888bde7dbfbc98d68774a750619615a77dfe10b6
Olivier Spinelli <olivier.spinelli@invenietis.com>
2015-07-20 12:41:16 +0200

    Evaluator simple tests.

---
+++ Analyse/ITI.Parser.Tests/EvaluationTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ITI.Parsing.Tests
+{
+    [TestFixture]
+    public class EvaluationTests
+    {
+        [TestCase( "6 - 4 + 5", -3.0 )]
+        [TestCase( "6 - (4 + 5)", -3.0 )]
+        [TestCase( "(6 - 4) + 5", 7.0 )]
+        public void simple_expression( string expression, double expected )
+        {
+            Analyser a = new Analyser();
+            Node e = a.Expression( expression );
+            double result = EvalVisitor.Evaluate( expression );
+            Assert.That( result, Is.EqualTo( expected ) );
+        }
+
+    }
+}
